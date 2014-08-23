4faa15437fd9a28fbdc2e7cfe67f88a00fc40725
Rex Feng <rexfeng@gmail.com>
2014-08-22 21:30:41 -0400

    add ruby 007

---
+++ answers/ruby/problem_007.rb
@@ -0,0 +1,11 @@
+# 10001st prime
+
+## By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+## What is the 10 001st prime number?
+
+require "prime"
+
+position = 10_001
+
+answer = Prime.first(position).last #=> 104743
