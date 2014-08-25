9ce4ee63f37cd0c60d308f476d42185918ca6baf
Rex Feng <rexfeng@gmail.com>
2014-08-25 01:54:54 -0400

    reduce a step in js 001

--- answers/javascript/problem_001.js
+++ answers/javascript/problem_001.js
@@ -7,17 +7,12 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
 var max = 1000;
-var multiples = [];
 var sum = 0;
 
 for (var i = 1; i < max; i++) {
   if((i%3===0) || (i%5===0)) {
-    multiples.push(i);
+    sum += i;
   }
-}
-
-for (i = 0; i < multiples.length; i++) {
-  sum += multiples[i];
 }
 
 console.log(sum); // 233168
